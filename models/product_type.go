1a155e28d297077203e28010b1d3d6551881f56d
Hữu Hà <business@zsmart.tech>
2021-09-19 03:02:40 +0700

    Add missing type

--- models/product_type.go
+++ models/product_type.go
@@ -1,6 +1,10 @@
 package models
 
+import "time"
+
 type ProductType struct {
-	ID   uint64 `gorm:"primaryKey;autoIncrement;not null;index"`
-	Name string `gorm:"type:character varying(20);not null;index"`
+	ID        uint64    `gorm:"primaryKey;autoIncrement;not null;index"`
+	Name      string    `gorm:"type:character varying(20);not null;index"`
+	CreatedAt time.Time `gorm:"type:timestamp(0);not null;index"`
+	UpdatedAt time.Time `gorm:"type:timestamp(0);not null;index"`
 }
